c27ed01307810b05b513c244c902a830ba4e70b2
Vasile <allfortrashmail@gmail.com>
2023-06-07 13:06:55 +0300

    implement signOut

--- lib/src/features/auth/infrastructure/github_authenticator.dart
+++ lib/src/features/auth/infrastructure/github_authenticator.dart
@@ -1,14 +1,20 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_github_viewer/core/shared/codecs.dart';
 import 'package:flutter_github_viewer/src/features/auth/domain/auth_failure.dart';
 import 'package:flutter_github_viewer/src/features/auth/infrastructure/credentials_storage/credentials_storage.dart';
 import 'package:oauth2/oauth2.dart';
 import 'package:http/http.dart' as http;
 
 class GithubAuthenticator {
-  GithubAuthenticator(this._credentialsStorage);
+  GithubAuthenticator(this._credentialsStorage, this._dio);
 
   final CredentialsStorage _credentialsStorage;
+  final Dio _dio;
 
   static const clientId = 'fdfd93229a852efc9b33';
   static const clientSecret = 'f392c19595ee987bf1105965fb988ed847af7def';
@@ -18,6 +24,8 @@ class GithubAuthenticator {
       Uri.parse('https://github.com/login/oauth/authorize');
   static final tokenEndpoint =
       Uri.parse('https://github.com/login/oauth/access_token');
+  static final revocationEndpoint =
+      Uri.parse('https://api.github.com/applications/$clientId/token');
   static final redirectUrl = Uri.parse('http://localhost:3000/callback');
 
   Future<Credentials?> getSignedInCredentials() async {
@@ -63,6 +71,40 @@ class GithubAuthenticator {
       return left(const AuthFailure.server());
     } on AuthorizationException catch (e) {
       return left(AuthFailure.server('${e.error}: ${e.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+
+  Future<Either<AuthFailure, Unit>> signOut() async {
+    final accessToken = await _credentialsStorage
+        .read()
+        .then((credentials) => credentials?.accessToken);
+
+    final usernameAndPassword =
+        stringToBase64.encode('$clientId:$clientSecret');
+
+    try {
+      try {
+        _dio.deleteUri(
+          revocationEndpoint,
+          data: {'accee_token': accessToken},
+          options: Options(
+            headers: {'Authorization': 'basic $usernameAndPassword'},
+          ),
+        );
+      } on DioError catch (e) {
+        if (e.type == DioErrorType.connectionError &&
+            e.error is SocketException) {
+          if (kDebugMode) {
+            print('SocketException: token not revoked');
+          }
+        } else {
+          rethrow;
+        }
+      }
+      await _credentialsStorage.clear();
+      return right(unit);
     } on PlatformException {
       return left(const AuthFailure.storage());
     }
