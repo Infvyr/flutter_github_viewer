8416a81350fb46547541a3890b04016bd1f8b867
Vasile <allfortrashmail@gmail.com>
2023-06-14 20:39:38 +0300

    create webview

--- lib/src/features/auth/presentation/authorization_page.dart
+++ lib/src/features/auth/presentation/authorization_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 @RoutePage()
 class AuthorizationPage extends StatefulWidget {
@@ -16,8 +17,22 @@ class AuthorizationPage extends StatefulWidget {
 }
 
 class _AuthorizationPageState extends State<AuthorizationPage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController();
+    _controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    _controller.loadRequest(Uri.parse(widget.authorizationUrl.toString()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
+      ),
+    );
   }
 }
