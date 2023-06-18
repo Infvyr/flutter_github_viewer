d8ce52980d36191ee51e914f241a39d703eb6874
Vasile <allfortrashmail@gmail.com>
2023-06-18 22:33:38 +0300

    prepare user dto

---
+++ lib/src/features/github/core/infrastructure/user_dto.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: invalid_annotation_target
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_dto.freezed.dart';
+part 'user_dto.g.dart';
+
+@freezed
+class UserDTO with _$UserDTO {
+  const UserDTO._();
+  const factory UserDTO({
+    @JsonKey(name: 'login') required String name,
+    @JsonKey(name: 'avatar_url') required String avatarUrl,
+  }) = _UserDTO;
+
+  factory UserDTO.fromJson(Map<String, dynamic> json) =>
+      _$UserDTOFromJson(json);
+
+  factory UserDTO.fromDomain(UserDTO user) => UserDTO(
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      );
+
+  UserDTO toDomain() => UserDTO.fromDomain(this);
+}
